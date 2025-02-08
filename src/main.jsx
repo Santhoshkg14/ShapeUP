b8942a6ebb6cdaef1f8254680a229e2d77c2bebd
Santhoshkg14 <santhoshkumar147gsk@gmail.com>
2025-02-08 21:27:53 +0530

    Added based name

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import Contact from './components/Contact/Contact.jsx'
 import Transform from './components/Transform/Transform.jsx'
 import Login from './components/Login/Login.jsx'
 
+const basename = import.meta.env.VITE_BASENAME || '/';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
@@ -25,7 +27,8 @@ const router = createBrowserRouter(
         <Route path=':userid' element={<User />} />
       </Route>      
     </Route>
-  )
+  ),
+  { basename }
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
